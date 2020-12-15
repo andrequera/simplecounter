b44575e44038e3c21adf9eef96a07f04785465d8
andrequera <andrequera@gmail.com>
2020-12-15 17:55:41 -0300

    ejercicio contador hr, min, seg

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-md-2">
+          <div className="card">
+
+            <div className="card-body">
+              <h5 className="card-title">{props.Hora1}</h5>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="card">
+
+            <div className="card-body">
+              <h5 className="card-title">{props.Hora0}</h5>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="card">
+
+            <div className="card-body">
+              <h5 className="card-title">{props.minuto1}</h5>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="card">
+
+            <div className="card-body">
+              <h5 className="card-title">{props.minuto0}</h5>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="card">
+
+            <div className="card-body">
+              <h5 className="card-title">{props.segundo1}</h5>
+
+            </div>
+          </div>
+        </div>
+        <div className="col-md-2">
+          <div className="card">
+
+            <div className="card-body">
+              <h5 className="card-title">{props.segundo0}</h5>
+
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+
+
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap"
+import "jquery"
+import "popper.js"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let hora0=0;
+let hora1=0;
+let min0=0;
+let min1=0;
+let seg0=0;
+let seg1=0;
+
+let contador = setInterval(()=>
+{
+  seg0++;
+  ReactDOM.render(<App segundo0={seg0} segundo1={seg1} minuto0={min0} minuto1={min1} Hora0={hora0} Hora1={hora1}/>, document.getElementById('root')
+)
+if (seg0===9){
+  seg0=0;
+  seg1++;
+}
+
+if (seg1===6){
+  seg1=0;
+  min0++;
+}
+
+if (min0===9){
+  min0=0;
+  min1++;
+}
+
+if (min1===6){
+  min1=0;
+  min1++;
+}
+
+if (hora0===4){
+  hora0=0;
+  hora1++;
+}
+
+if (hora1===2){
+  hora1=0;
+  clearInterval(contador);
+}
+
+}, 1);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
